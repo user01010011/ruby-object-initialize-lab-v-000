d8ac7960740308334139a063eaec0c0913741da8
Shasha Yan <shashayan@gmail.com>
2021-05-15 15:34:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,8 @@
 class Dog
 
-  def initialize(name)
+  def initialize(name, breed = "Mutt")
     @name = name
+    @breed = breed
   end
 
   def name=(name)
@@ -10,14 +11,6 @@ class Dog
 
   def name
     @name
-  end
-
-  def initialize(breed)
-    @breed = breed
-  end
-
-  def breed=(breed = "Mutt")
-    @breed = breed
   end
 
   def breed=(breed)
